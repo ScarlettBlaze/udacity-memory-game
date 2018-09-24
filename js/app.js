662992a0f3942c2cf187c8eadf1658e277eedb69
Andraea <andraea.camble@gmail.com>
2018-09-24 13:40:27 -0500

    Fixed end game state issue

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ for(let i = 0; i < pics.length; i++) {
 
         matchedCards.push(currentCard, previousCard); // Update matchedCards array.
 
-        //openCards = []; // Reset current pick and previous pick.
+        openCards = []; // Reset current pick and previous pick.
 
         // Is the game over?
         endGame();
@@ -55,7 +55,7 @@ for(let i = 0; i < pics.length; i++) {
         previousCard.classList.remove("open", "show");
         console.log("No match!");
 
-        //openCards = []; // Reset current pick and previous pick.
+        openCards = []; // Reset current pick and previous pick.
       }
     }
     // If there are no opened cards
@@ -83,7 +83,7 @@ function shuffle(array) {
 
 // End game.
 function endGame(){
-  if (matchedCards.length === openCards.length){
+  if (matchedCards.length === pics.length){
     alert("Game Over!")
   }
 }
